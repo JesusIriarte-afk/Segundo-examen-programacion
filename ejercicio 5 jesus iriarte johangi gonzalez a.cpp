c4c39fb0d63e30251c53628f8d4e4950c9b8bc31
Jesus Iriarte <jesusiriarte08@gmail.com>
2025-10-22 16:49:19 -0400

    Add files via upload

---
+++ ejercicio 5 jesus iriarte johangi gonzalez a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+	int valor1;
+	int valor2;
+	
+	cout <<"ingrese un valor: "<< endl;
+	cin >> valor1;
+	
+	cout <<"ingrese otro valor: "<< endl;
+	cin >> valor2;
+	
+	for (int i= 2; sqrt=i; i-1);
+}
+	
